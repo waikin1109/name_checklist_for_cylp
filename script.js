266200f9f86ca2c1cce0cefb8e1dc497854f5378
waikin1109 <waikin1109@hotmail.com>
2026-07-08 14:03:55 +0800

    Added API

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-let nameLists = ['Wai Kin', 'Steve', 'hary'];
+const API_URL = "https://script.google.com/macros/s/AKfycbz98B-WPaID73CgskBfFvutP7w2uiZvSnfTQh0Acc-u1HTFmywePEwIbHDzRIGTQhf_Xg/exec";
 
-let submitBtn = document.getElementById("submitBtn");
-let showtext = document.getElementById("showText");
-submitBtn.addEventListener("click", () => {
-    let input = document.getElementById("nameInput").value.trim().toLowerCase();
+const submitBtn = document.getElementById("submitBtn");
+const showText = document.getElementById("showText");
+const nameInput = document.getElementById("nameInput");
 
-    let found = nameLists.some(name => name.toLowerCase() === input);
+submitBtn.addEventListener("click", async () => {
 
-    if (found) {
-        showtext.textContent = (`Welcome ${input} !`);
-    } else {
-        showtext.textContent = ("Name not found!");
+    const input = nameInput.value.trim();
+
+    if (!input) {
+        showText.textContent = "Please enter your phone number.";
+        return;
+    }
+
+    showText.textContent = "Checking...";
+
+    try {
+        const response = await fetch(API_URL, {
+            method: "POST",
+            body: JSON.stringify({
+                input: input
+            })
+        });
+
+        const result = await response.json();
+
+        if (result.success) {
+            showText.textContent = `Welcome ${result.name}!`;
+            nameInput.value = "";
+        } else {
+            showText.textContent = result.message;
+        }
+
+    } catch (error) {
+        console.log(error);
+        showText.textContent = "User not found.";
     }
 });
